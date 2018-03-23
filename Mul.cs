d9cc38129678d221672ac9dd0218c4336c99de42
Brandon Yen <byen@vols.utk.edu>
2018-03-23 11:40:20 -0400

    Implemented Mul

--- Mul.cs
+++ Mul.cs
@@ -2,4 +2,14 @@ using System;
 
 public class Mul : IInstruction
 {
+    public Mul(){}
+    
+    public uint Val{
+        get{
+            uint retVal = 1 << 4;
+            retVal += 1;
+            retVal <<= 25;
+            return retVal;
+        }
+    }
 }

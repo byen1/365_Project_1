a1a7d2f61d6ebb61aa748dd770807a5a46bdf591
Brandon Yen <byen@vols.utk.edu>
2018-03-23 12:06:25 -0400

    Implemented And

---
+++ And.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class And : IInstruction
+{
+    public And(){}
+    
+    public uint Val{
+        get{
+            uint retVal = 1 << 3;
+            ++retVal;
+            retVal <<= 2;
+			++retVal;
+			retVal <<= 24;
+            return retVal;
+        }
+    }
+}

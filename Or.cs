ce7378ad9fde1076034d151c2ea85f3200d458c1
Brandon Yen <byen@vols.utk.edu>
2018-03-23 12:11:54 -0400

    Implemented Or

---
+++ Or.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Or : IInstruction
+{
+    public Or(){}
+    
+    public uint Val{
+        get{
+            uint retVal = 1 << 3;
+            ++retVal;
+            retVal <<= 1;
+			++retVal;
+			retVal <<= 25;
+            return retVal;
+        }
+    }
+}

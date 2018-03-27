b866d3baf1f53765c5a80ee90f909f35ba3cbcda
byen1 <35308000+byen1@users.noreply.github.com>
2018-03-27 13:03:08 -0400

    Commented And.cs
    Brief description + header

--- And.cs
+++ And.cs
@@ -1,3 +1,16 @@
+/*
+*	CS365 Project 1
+*
+*	Contributors:
+*		Austyn Simons
+*		Brandon Yen
+*		Charles Rizzo
+*
+*	And.cs creates an object of type And that has a read-only Val property. The Val property gets the byte encoding of the and	
+*	command.
+*
+*/
+
 using System;
 
 public class And : IInstruction

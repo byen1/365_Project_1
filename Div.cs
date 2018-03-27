51d2d321176714264fc1c8aa8a8eda984b487ef2
byen1 <35308000+byen1@users.noreply.github.com>
2018-03-27 12:43:48 -0400

    Commented Div.cs
    Brief description + header

--- Div.cs
+++ Div.cs
@@ -1,3 +1,16 @@
+/*
+*	CS365 Project 1
+*
+*	Contributors:
+*		Austyn Simons
+*		Brandon Yen
+*		Charles Rizzo
+*
+*	Div.cs creates an object of type Div that has a read-only Val property. The Val property gets the byte encoding of the div	
+*	command.
+*
+*/
+
 public class Div : IInstruction{
     public Div(){}
     

9375336c43444c33ae853321167934d570aed657
byen1 <35308000+byen1@users.noreply.github.com>
2018-03-27 12:49:30 -0400

    Commented Neg.cs
    Brief description + header

--- Neg.cs
+++ Neg.cs
@@ -1,3 +1,16 @@
+/*
+*	CS365 Project 1
+*
+*	Contributors:
+*		Austyn Simons
+*		Brandon Yen
+*		Charles Rizzo
+*
+*	Neg.cs creates an object of type Neg that has a read-only Val property. The Val property gets the byte encoding of the neg	
+*	command.
+*
+*/
+
 using System;
 
 public class Neg : IInstruction

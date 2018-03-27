615bb3f39e49f1f59605c285836439ed14f407ca
byen1 <35308000+byen1@users.noreply.github.com>
2018-03-27 13:06:41 -0400

    Commented Rem.cs
    Brief description + header

--- Rem.cs
+++ Rem.cs
@@ -1,3 +1,16 @@
+/*
+*	CS365 Project 1
+*
+*	Contributors:
+*		Austyn Simons
+*		Brandon Yen
+*		Charles Rizzo
+*
+*	Rem.cs creates an object of type Rem that has a read-only Val property. The Val property gets the byte encoding of the rem	
+*	command.
+*
+*/
+
 public class Rem : IInstruction{
     public Rem(){}
     

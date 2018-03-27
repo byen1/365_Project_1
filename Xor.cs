e3934cbbeacfb939028fe62b22c42c57fc2f19d1
byen1 <35308000+byen1@users.noreply.github.com>
2018-03-27 13:09:39 -0400

    Commented Xor.cs
    Brief Description + header

--- Xor.cs
+++ Xor.cs
@@ -1,3 +1,16 @@
+/*
+*	CS365 Project 1
+*
+*	Contributors:
+*		Austyn Simons
+*		Brandon Yen
+*		Charles Rizzo
+*
+*	Xor.cs creates an object of type Xor that has a read-only Val property. The Val property gets the byte encoding of the xor	
+*	command.
+*
+*/
+
 using System;
 
 public class Xor : IInstruction
